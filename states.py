285b649580b53e02b7204c4bc98085239a0bdf53
Prentice Jones <pj2358@barnard.edu>
2023-05-09 23:49:30 +0200

    created barebones contianer class for actions but need to add iadd for contianers

--- states.py
+++ states.py
@@ -1,7 +1,10 @@
+"""Used for defining states"""
+
+
 class States:
-    "Used for defining states"""
+
     def __int__(self, state):
         self.state = state
+
     def __eq__(self, other):
         return self.state == other.state
-
---
+++ states_container.py
@@ -0,0 +1,17 @@
+"""This is a container for states"""
+
+
+class States_Cont:
+    def __int__(self):
+        """A container can be initialized empty"""
+
+    def __len__(self):
+        """Will return the number of states in a container"""
+        return len(self)
+
+    def __iter__(self):
+        """Will return an iterator over the states in a container"""
+        return self
+
+    def __next__(self):
+        """Will return the next states in a container"""
