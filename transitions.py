f0a93635a48e6bf2b15e97762cfe9cd548dc492f
Lucia Perez Saignac <lip2110@columbia.edu>
2023-05-11 15:20:55 +0200

    classes and functions added to transitions.py

---
+++ transitions.py
@@ -0,0 +1,22 @@
+
+
+class MDPAction:
+    def __init__(self, id : str, p : float):
+
+class MDPTranstion:
+    def __init__(self, start_state: MDPState, action : MDPAction, next_state: MDPState):
+
+
+class MDPTransitions:
+    def __init__(self):
+
+    def __len__(self):
+
+    def __iter__(self):
+
+    def __next__(self):
+
+
+
+
+
