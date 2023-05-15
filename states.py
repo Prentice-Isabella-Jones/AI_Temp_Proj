9226fd8753aba19c24abf2034bac54d9374fd6b4
Prentice Jones <pj2358@barnard.edu>
2023-05-15 14:16:30 +0200

    modified main to use state_container obj which is the list

--- states.py
+++ states.py
@@ -3,8 +3,8 @@
 
 class States:
 
-    def __int__(self, state):
+    def __int__(self, state: int) -> int:
         self.state = state
 
-    def __eq__(self, other):
+    def __eq__(self, other: int) -> int:
         return self.state == other.state
